5379a6c2211282030442ff03b33b1e9de7ef640f
prazwalayyyyyyyy <75473235+prazwalayyyyyyyy@users.noreply.github.com>
2024-02-06 16:16:56 +0545

    Time: 30 ms (93.63%) | Memory: 16.5 MB (74.07%) - LeetSync

---
+++ Leetcode75/1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        # Check if concatenated strings are equal or not, if not return ""
+        if str1 + str2 != str2 + str1:
+            return ""
+        
+        # If strings are equal, return the substring from 0 to gcd of size(str1), size(str2)
+        from math import gcd
+        return str1[:gcd(len(str1), len(str2))]
+
+# Create an instance of the Solution class
+sol = Solution()
+
+# Example usage
+result = sol.gcdOfStrings("ABCABC", "ABC")
+print(result)
